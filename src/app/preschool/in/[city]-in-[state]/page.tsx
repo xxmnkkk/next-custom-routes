cffdfbe62a1c1921e0e7fa4f4ff5962e2ba97512
Abhishek Saini <binnu24668@gmail.com>
2024-10-19 18:21:28 +0530

    demo app for custom pages

---
+++ src/app/preschool/in/[city]-in-[state]/page.tsx
@@ -0,0 +1,14 @@
+export default function PreschoolInCityInState({ params }) {
+  const { city, state } = params;
+
+  return (
+    <div>
+      <h1>
+        Preschools in {city}, {state}
+      </h1>
+      <p>
+        Here you will find preschools located in {city}, {state}.
+      </p>
+    </div>
+  );
+}
---
+++ src/app/preschool/in/india/page.tsx
@@ -0,0 +1,8 @@
+export default function PreschoolInIndia() {
+  return (
+    <div>
+      <h1>Preschools in India</h1>
+      <p>Here you will find preschools located across India.</p>
+    </div>
+  );
+}
